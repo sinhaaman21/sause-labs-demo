ebd3688a0a35bd96f13483ddcbad8716713c8092
sinhaaman21 <sinha.amansinha@gmail.com>
2024-07-10 13:10:32 +0530

    code refactor to use fixtures

--- cypress/e2e/step_definations/steps.js
+++ cypress/e2e/step_definations/steps.js
@@ -1,15 +1,23 @@
 import { Given, When, Then, After, And} from "cypress-cucumber-preprocessor/steps";
 import { locators } from "../../support/locators";
 
+let testData;
+
+before(() => {
+  cy.fixture('testData').then((data) => {
+    testData = data;
+  });
+});
+
 Given('the user is on the Sauce Labs login page', () => {
-  cy.visit('https://www.saucedemo.com/');
+  cy.visit(testData.baseUrl);
 });
 
 When('the user logs in with valid credentials', () => {
-  cy.get(locators.username).type('standard_user');
-  cy.get(locators.password).type('secret_sauce');
+  cy.get(locators.username).type(testData.username);
+  cy.get(locators.password).type(testData.password);
   cy.get(locators.loginButton).click();
-  cy.url().should('include', '/inventory.html');
+  cy.url().should('include', testData.homePageUrl);
 });
 
 When('the user selects 3 random items', () => {
@@ -24,24 +32,24 @@ When('the user selects 3 random items', () => {
 
 When('the user proceeds to checkout', () => {
   cy.get(locators.cartLink).click();
-  cy.url().should('include', '/cart.html');
+  cy.url().should('include', testData.cartUrl);
   cy.get(locators.checkoutButton).should('be.visible').click();
-  cy.url().should('include', '/checkout-step-one.html');
+  cy.url().should('include', testData.checkoutStepOneUrl);
 });
 
 When('the user fills in their information', () => {
-  cy.get(locators.firstName).type('John');
-  cy.get(locators.lastName).type('Doe');
-  cy.get(locators.postalCode).type('12345');
+  cy.get(locators.firstName).type(testData.firstName);
+  cy.get(locators.lastName).type(testData.lastName);
+  cy.get(locators.postalCode).type(testData.postalCode);
   cy.get(locators.continueButton).click();
-  cy.url().should('include', '/checkout-step-two.html');
+  cy.url().should('include', testData.checkoutStepTwoUrl);
 });
 
 When('the user completes the checkout', () => {
   cy.get(locators.finishButton).click();
-  cy.url().should('include', '/checkout-complete.html');
+  cy.url().should('include', testData.checkoutCompleteUrl);
 });
 
 Then('the user should see a successful checkout message', () => {
-  cy.get(locators.completeHeader).should('contain', 'Thank you for your order!');
+  cy.get(locators.completeHeader).should('contain', testData.checkoutMsg);
 });
